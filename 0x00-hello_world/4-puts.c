7042981e914595f3194ac38c44aa07722fed8054
elgrandesoso <soufianelkbir23@gmail.com>
2023-07-14 09:31:21 -0700

    5. Hello, printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 /**
- * 
  *main -  entry point
- return : always 0 (sucsses)
-*
+ *return : always 0 (sucsses)
 */
 int main(void)
 {
-puts( "\"Programming is like building a multilingual puzzle" );
+puts("\"Programming is like building a multilingual puzzle");
 return(0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
-#include <stdio.>
+#include <stdio.h>
 /**
-*main - Entry Point
-*Return always 0 (seccess)
+main - Entry Point
+Return always 0 (seccess)
 */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+printf( "with proper grammar, but the outcome is a piece of art,\n");
+return( 0 );
 }
