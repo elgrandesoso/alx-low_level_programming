18bd5591baa2b2a58ca16231d5c19378299e67b5
elgrandesoso <soufianelkbir23@gmail.com>
2023-07-18 14:16:55 -0700

    1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,14 @@
 #include "main.h"
 
 
-	/**
-	 * print_alphabet - print all alphabet in lowercase
-	 */
-
-
+/**
+ * main - Entry point of the program
+ *
+ * writen by soufiane el kbir 
+ * Description: rototype: void print_alphabet(void);
+You can only use _putchar twice in your code.
+ * Return: 0 on successful execution
+ */
 	void print_alphabet(void)
 	{
 		char letter;
