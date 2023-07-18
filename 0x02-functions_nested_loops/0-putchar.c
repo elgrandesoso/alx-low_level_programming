b6044e5855b5d62ac7b42cbbb7363f1110062d7a
elgrandesoso <soufianelkbir23@gmail.com>
2023-07-18 10:46:40 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,13 @@
-#include "main.h"
+#include <stdio.h>
+
 /**
- * @brief This is the entry point of the program.
- *        It prints "_putchar" followed by a new line.
- *
- * @return int Returns 0 on successful execution.
- * writen by Soufiane el kbir 
+ * main - Entry point of the program
+ * writen by soufiane el kbir 
+ * Description: This function prints "_putchar" followed by a new line.
+ * Return: 0 on successful execution
  */
-	int main(void)
-	{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
-	}
+int main(void)
+{
+    printf("_putchar\n");
+    return 0;
+}
